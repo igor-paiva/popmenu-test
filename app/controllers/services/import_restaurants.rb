70419ab8cc53b31e743cd994f1f01bd539483403
igor-paiva <igorbatistapaiva@outlook.com>
2025-05-28 17:59:30 -0300

    change upserts to use defined unique by keys

--- app/controllers/services/import_restaurants.rb
+++ app/controllers/services/import_restaurants.rb
@@ -1,7 +1,7 @@
 module Services
   class ImportRestaurants
     def initialize(params)
-      @params = params
+      @params = params.to_h
     end
 
     def run
@@ -22,13 +22,18 @@ module Services
       def extract_restaurants!
         @restaurants_data = @params[:restaurants]
 
+        unique_by = Array.wrap(restaurants_unique_by)
+        returning = unique_by + %i[id]
+
         result = Restaurant.upsert_all(
-          @restaurants_data.map { _1.slice(:name) }, unique_by: :name, returning: %i[name id]
+          @restaurants_data.map { _1.slice(:name) }, unique_by:, returning:
         ).to_a
 
         raise ActiveRecord::Rollback if result.length != @restaurants_data.length
 
-        @restaurants_map = result.map { [ _1["name"], _1["id"] ] }.to_h
+        hash_map = result.map { [ _1.fetch_values(*unique_by.map(&:to_s)), _1["id"] ] }
+
+        @restaurants_map = hash_map.to_h
       end
 
       def extract_menus!
@@ -38,55 +43,77 @@ module Services
 
             menu_data[:menu_items] = menu[:dishes] if menu_data[:menu_items].blank? && menu.key?(:dishes)
 
-            menu_data.merge(restaurant_id: @restaurants_map[restaurant[:name]])
+            menu_data.merge(restaurant_id: @restaurants_map[restaurant.fetch_values(*restaurants_unique_by)])
           end
         end
 
+        unique_by = Array.wrap(menus_unique_by)
+        returning = unique_by + %i[id]
+
         result = Menu.upsert_all(
-          @menus_data.map { _1.slice(*%i[name description restaurant_id]) },
-          unique_by: %i[name restaurant_id],
-          returning: %i[restaurant_id name id]
+          @menus_data.map { _1.slice(*%i[name description restaurant_id]) }, unique_by:, returning:
         ).to_a
 
         raise ActiveRecord::Rollback if result.length != @menus_data.length
 
-        @menus_map = result.map { [ [ _1["restaurant_id"], _1["name"] ], _1["id"] ] }.to_h
+        hash_map = result.map { [ _1.fetch_values(*unique_by.map(&:to_s)), _1["id"] ] }
+
+        @menus_map = hash_map.to_h
       end
 
       def extract_menu_items!
         @menu_items_data = @menus_data.flat_map do |menu|
           menu[:menu_items].map do |menu_item|
-            menu_item.slice(*%i[name description price]).merge(menu_id: @menus_map[[ menu[:restaurant_id], menu[:name] ]])
+            menu_item.slice(*%i[name description price]).merge(
+              menu_id: @menus_map[menu.fetch_values(*menus_unique_by)]
+            )
           end
         end
 
+        unique_by = Array.wrap(menu_items_unique_by)
+        returning = unique_by + %i[id]
+
         result = MenuItem.upsert_all(
-          @menu_items_data.map { _1.slice(*%i[name description price]) },
-          unique_by: :name,
-          returning: %i[name id]
+          @menu_items_data.map { _1.slice(*%i[name description price]) }, unique_by:, returning:
         ).to_a
 
         raise ActiveRecord::Rollback if result.length != @menu_items_data.length
 
-        @menu_items_map = result.map { [ _1["name"], _1["id"] ] }.to_h
+        hash_map = result.map { [ _1.fetch_values(*unique_by.map(&:to_s)), _1["id"] ] }
+
+        @menu_items_map = hash_map.to_h
       end
 
       def extract_menu_menu_items!
         menu_menu_items_data = @menu_items_data.map do |menu_item|
           {
             menu_id: menu_item[:menu_id],
-            menu_item_id: @menu_items_map[menu_item[:name]],
+            menu_item_id: @menu_items_map[menu_item.fetch_values(*menu_items_unique_by)],
             price: menu_item[:price]
           }
         end
 
         result = MenuMenuItem.upsert_all(
-          menu_menu_items_data,
-          unique_by: %i[menu_id menu_item_id],
-          returning: :id
+          menu_menu_items_data, unique_by: menu_menu_items_unique_by, returning: :id
         )
 
         raise ActiveRecord::Rollback if result.length != menu_menu_items_data.length
+      end
+
+      def restaurants_unique_by
+        %i[name]
+      end
+
+      def menus_unique_by
+        %i[name restaurant_id]
+      end
+
+      def menu_items_unique_by
+        %i[name]
+      end
+
+      def menu_menu_items_unique_by
+        %i[menu_id menu_item_id]
       end
   end
 end
