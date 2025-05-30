029dac9d0e22d7b6ed9a90743b74e8eb3b920145
igor-paiva <igorbatistapaiva@outlook.com>
2025-05-29 22:27:49 -0300

    handle application exceptions and return internal server error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  rescue_from StandardError, with: :internal_server_error
+
+  private
+
+    def internal_server_error(error)
+      Rails.logger.error(error)
+
+      render json: { error: "Internal server error" }, status: :internal_server_error
+    end
 end
